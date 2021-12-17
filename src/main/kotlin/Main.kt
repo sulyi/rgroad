120b6e8e6dfa234a233dca437c6d1b1901c0ac70
sulyi <sulyi.gbox@gmail.com>
2021-12-17 21:44:13 +0100

    Add dummy application to main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RgRoadApplication
+
+
 fun main(args: Array<String>) {
+    runApplication<RgRoadApplication>(*args)
 }
